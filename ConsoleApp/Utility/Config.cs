cb80ccf67bb5f1697ba3e57cee26bbde9c7bb6ad
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-10 17:56:42 +0100

    Docs: add clarifying comments to config file

--- ConsoleApp/Utility/Config.cs
+++ ConsoleApp/Utility/Config.cs
@@ -2,18 +2,23 @@
 {
     public class Config
     {
+        // Constants as per specifications
         public const int MinNumber = 10;
         public const int MaxNumber = 9999;
         public const int TextMinLength = 5;
         public const int TextMaxLength = 45;
 
+        // Max number of elements in array list
         public const int MaxNumberOfElements = 7;
 
+        // String to be attached on input text processing
         public const string OutputString = "Making an impact that matters –Deloitte";
 
+        // The min and max values of the ascii table, used in random string generation
         public const int AsciiMin = 0;
         public const int AsciiMax = 126;
 
+        // The Digits provided as choices during input type choice method
         public const int NumberInputDigit = 1;
         public const int TextInputDigit = 2;
     }
