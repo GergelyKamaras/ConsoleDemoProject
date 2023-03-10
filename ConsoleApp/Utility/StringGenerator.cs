12524948ee950acb93299bc1f40c417a1e60412e
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-10 12:45:35 +0100

    Add: string generator for testing

---
+++ ConsoleApp/Utility/StringGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp.Utility
+{
+    public static class StringGenerator
+    {
+        private const int _ascciMin = 0;
+        private const int _asciiMax = 126;
+        
+        public static string GenerateString(int length)
+        {
+            Random random = new Random();
+            string outString = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                outString += Convert.ToChar(random.Next(_ascciMin, _asciiMax));
+            }
+
+            return outString;
+        }
+    }
+}
