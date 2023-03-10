09932781950c537b144cd6ddbcbeba1bf0c98a37
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-10 11:06:45 +0100

    Add: UI class, welcome message

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ConsoleApp
+﻿using ConsoleApp.View;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            UI ui = new UI();
+
+            ui.Welcome();
         }
     }
 }
---
+++ ConsoleApp/View/UI.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp.View
+{
+    internal class UI
+    {
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to this console application!");
+            Console.WriteLine("Please follow the onscreen instructions!");
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
