770e64e475adc54e7f3b3e99248fccce815cbab3
Gergely Kamarás <gergely.kamaras@gmail.com>
2023-03-10 11:38:09 +0100

    Add: DataCache class for storing the input

---
+++ ConsoleApp/Model/DataCache.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace ConsoleApp.Model
+{
+    internal class DataCache
+    {
+        public const int MaxNumberOfElements = 7;
+        public ArrayList Elements = new ArrayList();
+    }
+}
